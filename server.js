1cb022480f0f8b0a1cafb5668ca2643382257ed8
pirate4593 <joconnor10@student.cvtc.edu>
2023-02-14 10:50:52 -0600

    Revised code to match Standard Style

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const newFolderName = function (folders) {
-  let n = folders.length
-  if (folders.includes("New Folder") && !folders.includes("New Folder (2)")) {
-    folders.push("New Folder (2)")
+  const n = folders.length
+  if (folders.includes('New Folder') && !folders.includes('New Folder (2)')) {
+    folders.push('New Folder (2)')
   }
-  if (!folders.includes("New Folder")) {
-    folders.push("New Folder")
+  if (!folders.includes('New Folder')) {
+    folders.push('New Folder')
   }
   if (n === 0) {
-    folders.push("New Folder")
-  }  
-
-  if (folders.includes("New Folder (" + n + ")") && !folders.includes("New Folder (" + (n + 1) + ")")) {
-    folders.push("New Folder (" + (n + 1) + ")")
+    folders.push('New Folder')
   }
-  if (!folders.includes("New Folder (" + n + ")") && folders.includes("New Folder (" + (n + 1) + ")")) {
-    folders.push("New Folder (" + n + ")")
+  if (folders.includes('New Folder (' + n + ')') && !folders.includes('New Folder (' + (n + 1) + ')')) {
+    folders.push('New Folder (' + (n + 1) + ')')
+  }
+  if (!folders.includes('New Folder (' + n + ')') && folders.includes('New Folder (' + (n + 1) + ')')) {
+    folders.push('New Folder (' + n + ')')
   }
 
   return folders[n]
